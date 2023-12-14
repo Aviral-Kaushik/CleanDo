b325ea6e9c6274bdf6a6a38186304137081825a0
Aviral Kaushik <zaviralkaushik@gmail.com>
2023-12-14 20:31:54 +0530

    Added Composable for DefaultRadioButton

--- app/src/main/java/com/aviral/cleando/di/AppModule.kt
+++ app/src/main/java/com/aviral/cleando/di/AppModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.aviral.cleando.featureNotes.data.dataSource.NoteDatabase
 import com.aviral.cleando.featureNotes.data.repository.NoteRepositoryImplementation
 import com.aviral.cleando.featureNotes.domain.repository.NoteRepository
+import com.aviral.cleando.featureNotes.domain.useCase.AddNote
 import com.aviral.cleando.featureNotes.domain.useCase.DeleteNote
 import com.aviral.cleando.featureNotes.domain.useCase.GetNotes
 import com.aviral.cleando.featureNotes.domain.useCase.NoteUseCases
@@ -38,7 +39,8 @@ class AppModuleImplementation(
     override val noteUseCases: NoteUseCases by lazy {
         NoteUseCases(
             getNotes = GetNotes(notesRepository),
-            deleteNote = DeleteNote(notesRepository)
+            deleteNote = DeleteNote(notesRepository),
+            addNote = AddNote(notesRepository)
         )
     }
 
