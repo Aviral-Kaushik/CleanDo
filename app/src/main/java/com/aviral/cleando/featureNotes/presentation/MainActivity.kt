482adfe48de98a2589b5b737877f13f29478a5d8
Aviral Kaushik <zaviralkaushik@gmail.com>
2023-12-17 17:54:44 +0530

    Updated To-Dos

--- app/src/main/java/com/aviral/cleando/featureNotes/presentation/MainActivity.kt
+++ app/src/main/java/com/aviral/cleando/featureNotes/presentation/MainActivity.kt
@@ -6,6 +6,12 @@ import androidx.activity.compose.setContent
 import com.aviral.cleando.ui.theme.CleanDoTheme
 
 class MainActivity : ComponentActivity() {
+
+    /**
+     * TODO Try to figure out the SavedStateHandle that Dragger Hilt automatically handles and put it
+     *      as parameter to AddEditNoteViewModer
+     * */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
